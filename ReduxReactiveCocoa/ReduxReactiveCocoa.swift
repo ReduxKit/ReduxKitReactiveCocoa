9e73e89659cd40d9d2ddaef34bee252e286d34a4
Karl Bowden <karl@karlbowden.com>
2015-12-20 13:19:47 +1100

    Export and test createStore, createStateStream and createDisposable
    Signed-off-by: Karl Bowden <karl@karlbowden.com>

--- ReduxReactiveCocoa/ReduxReactiveCocoa.swift
+++ ReduxReactiveCocoa/ReduxReactiveCocoa.swift
@@ -8,3 +8,52 @@
 
 import ReactiveCocoa
 import Redux
+
+
+/**
+
+ Uses `createStateStream` to create a `Redux.Store<State>` using a
+ `ReactiveCocoa.Observer<State, NoError>` stream.
+
+ */
+public func createStore<State>(reducer: ((State?, Redux.Action) -> State), state: State? = nil) -> Store<State> {
+    return createStreamStore(createStream, reducer: reducer, state: nil)
+}
+
+
+/**
+
+ Accepts a `State` and returns `Redux.StateStream<State>` using a
+ `ReactiveCocoa.Observer<State, NoError>` as the stream provider.
+
+ */
+public func createStream<State>(state: State) -> StateStream<State> {
+
+    var latest: State = state
+    let (signal, observer) = Signal<State, NoError>.pipe()
+
+    signal.observeNext { latest = $0 }
+    observer.sendNext(state)
+
+    func subscribe(subscriber: State -> ()) -> ReduxDisposable {
+        subscriber(latest)
+        return createDisposable(signal.observeNext(subscriber))
+    }
+
+    return StateStream(dispatch: observer.sendNext, subscribe: subscribe, getState: { latest })
+}
+
+
+/**
+
+ Accepts a `ReactiveCocoa.Disposable` and returns it wrapped as a `ReduxDisposable`.
+
+ */
+public func createDisposable(rawDisposable: Disposable?) -> ReduxDisposable {
+
+    if let disposable = rawDisposable {
+        return SimpleReduxDisposable(disposed: { disposable.disposed }, dispose: disposable.dispose)
+    }
+
+    return SimpleReduxDisposable(disposed: { false }, dispose: {})
+}
